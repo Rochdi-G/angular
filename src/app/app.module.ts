21e5083c26c50dd415bbb3762c1dbbaffb43ca49
Rochdi-G <rochdigammoudi@gmail.com>
2017-12-25 21:20:34 +0100

    basic services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
-
+import { DishService } from './services/dish.service';
 
 @NgModule({
   declarations: [
@@ -26,7 +26,7 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     FlexLayoutModule
 
   ],
-  providers: [],
+  providers: [ DishService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
