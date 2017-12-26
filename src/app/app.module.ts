ead6478807bebde3b365ebbe5b0ffb648081019a
Rochdi-G <rochdigammoudi@gmail.com>
2017-12-26 01:33:31 +0100

    single pg app pt1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 
 @NgModule({
   declarations: [
@@ -39,7 +40,8 @@ import { DishService } from './services/dish.service';
     FlexLayoutModule,
     AppRoutingModule
   ],
-  providers: [ DishService ],
+  providers: [ DishService,
+  PromotionService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/promotion.ts
@@ -0,0 +1,9 @@
+export class Promotion {
+    id: number;
+    name: string;
+    label: string;
+    price: string;
+    image: string;
+    featured: boolean;
+    description: string;
+}
